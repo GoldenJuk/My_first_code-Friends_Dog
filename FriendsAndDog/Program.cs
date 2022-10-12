55c340adf20f534d32006c523520bd526c227f2e
Alex Kizilov <as_k@list.ru>
2022-10-12 19:46:50 +0400

    My first code

---
+++ FriendsAndDog/Program.cs
@@ -0,0 +1,20 @@
+﻿int
+    count = 0, friend = 2;
+double 
+    distance = 10000, firstFriendSpeed = 1, secondFriendSpeed = 2, dogSpeed = 5, time;
+    while  (distance > 10)
+    {
+        if (friend == 1)
+        {
+            time = distance / (firstFriendSpeed + dogSpeed);
+            friend = 2;
+        }
+        else
+        {
+            time = distance / (secondFriendSpeed + dogSpeed);
+            friend = 1;
+        }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+    }
+Console.WriteLine(count);
